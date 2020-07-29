ac6c9f514fdb7eb9aec7db6f31ba207d12a53968
unknown <brownnguyen268@gmail.com>
2020-07-29 16:58:33 +0700

    add_backtop

--- js/index.js
+++ js/index.js
@@ -7,6 +7,14 @@ window.onscroll = function () {
     else {
         header.classList.remove('stick')
     }
+    if (window.pageYOffset > window.innerHeight / 2) {
+        document.querySelector('#backTop').style.opacity = 0.8;
+        document.querySelector('#backTop').style.visibility = "visible"
+    }
+    else {
+        document.querySelector('#backTop').style.opacity = 0;
+        document.querySelector('#backTop').style.visibility = "hidden"
+    }
 }
 let menu = document.querySelector('.menu');
 let drawer = document.querySelector('.drawer');
@@ -32,38 +40,45 @@ layerDrawer.onclick = function () {
 
 
 const body = document.body,
-scrollWrap = document.getElementsByClassName("main-body")[0],
-height = scrollWrap.getBoundingClientRect().height - 1,
-speed = 0.04;
+    scrollWrap = document.getElementsByClassName("main-body")[0],
+    height = scrollWrap.getBoundingClientRect().height - 1,
+    speed = 0.04;
 
 var offset = 0;
 
 body.style.height = Math.floor(height) + "px";
 
 function smoothScroll() {
-offset += (window.pageYOffset - offset) * speed;
+    offset += (window.pageYOffset - offset) * speed;
 
-var scroll = "translateY(-" + offset + "px) translateZ(0)";
-// scrollWrap.style.transform = scroll;
+    var scroll = "translateY(-" + offset + "px) translateZ(0)";
+    // scrollWrap.style.transform = scroll;
 
-callScroll = requestAnimationFrame(smoothScroll);
+    callScroll = requestAnimationFrame(smoothScroll);
 }
 
 smoothScroll();
 
 $(function () {
-var elements = $(".block-white, .block-black").toArray();
-console.log(elements)
-$(window).scroll(function () {
+    var elements = $(".block-white, .block-black").toArray();
+    console.log(elements)
+    $(window).scroll(function () {
+        elements.forEach(function (item) {
+            if ($(this).scrollTop() >= $(item).offset().top - 600) {
+                $(item).addClass("rev");
+            }
+        });
+    });
     elements.forEach(function (item) {
         if ($(this).scrollTop() >= $(item).offset().top - 600) {
             $(item).addClass("rev");
         }
     });
 });
-elements.forEach(function (item) {
-    if ($(this).scrollTop() >= $(item).offset().top - 600) {
-        $(item).addClass("rev");
-    }
-});
+
+$('#backTop').click(function (event) {
+    $('html,body').animate({
+        scrollTop: 0
+    }, 1000, 'swing');
+    return false;
 });

a53157e9f35f82ad89a021f4904f34e3a5fa7d37
unknown <brownnguyen268@gmail.com>
2020-07-29 16:49:38 +0700

    add_animate_2

--- js/index.js
+++ js/index.js
@@ -52,17 +52,17 @@ callScroll = requestAnimationFrame(smoothScroll);
 smoothScroll();
 
 $(function () {
-var elements = $(".block-rev").toArray();
+var elements = $(".block-white, .block-black").toArray();
 console.log(elements)
 $(window).scroll(function () {
     elements.forEach(function (item) {
-        if ($(this).scrollTop() >= $(item).offset().top - 400) {
+        if ($(this).scrollTop() >= $(item).offset().top - 600) {
             $(item).addClass("rev");
         }
     });
 });
 elements.forEach(function (item) {
-    if ($(this).scrollTop() >= $(item).offset().top - 400) {
+    if ($(this).scrollTop() >= $(item).offset().top - 600) {
         $(item).addClass("rev");
     }
 });

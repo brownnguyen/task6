3c571f74af6035114f96d9c6cc47777c9528b56e
unknown <brownnguyen268@gmail.com>
2020-07-30 10:12:05 +0700

    add

--- js/index.js
+++ js/index.js
@@ -38,7 +38,6 @@ layerDrawer.onclick = function () {
     layerDrawer.classList.remove("expand");
 }
 
-
 const body = document.body,
     scrollWrap = document.getElementsByClassName("main-body")[0],
     height = scrollWrap.getBoundingClientRect().height - 1,

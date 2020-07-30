2bf6e8f52534fd114e07b878d5c8b4f54c82992e
unknown <brownnguyen268@gmail.com>
2020-07-30 10:40:53 +0700

    add

--- js/index.js
+++ js/index.js
@@ -43,21 +43,6 @@ const body = document.body,
     height = scrollWrap.getBoundingClientRect().height - 1,
     speed = 0.04;
 
-var offset = 0;
-
-body.style.height = Math.floor(height) + "px";
-
-function smoothScroll() {
-    offset += (window.pageYOffset - offset) * speed;
-
-    var scroll = "translateY(-" + offset + "px) translateZ(0)";
-    // scrollWrap.style.transform = scroll;
-
-    callScroll = requestAnimationFrame(smoothScroll);
-}
-
-smoothScroll();
-
 $(function () {
     var elements = $(".block-white, .block-black").toArray();
     $(window).scroll(function () {
